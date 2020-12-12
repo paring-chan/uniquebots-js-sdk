26bbaee67d6c3cdc7ca29830222e27af71fd09b1
pikokr <parangee9706@gmail.com>
2020-12-12 22:50:39 +0900

    backup

--- test/src/self.js
+++ test/src/self.js
@@ -4,4 +4,4 @@ const config = require('../config.json')
 
 const bot = new UniqueBot(config.uniqueBotsToken)
 
-bot.updateGuilds(10).then(res => console.log(res))
+bot.updateGuilds(1).then(res => console.log(res))
